c9cd256be4d77ec4f8dc402d0d5954eec2fd6e69
Mu-munn <rpgmukai333@gmail.com>
2022-11-10 22:24:38 +0900

    axiosによるapi実装

---
+++ src/api/user.ts
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+const ENDPOINT_URL = "https://qiita.com/api/v2/items?query=$Mukai%20Yuuki"
+
+type User = {
+  id: string
+  name: string
+}
+
+const userApi = {
+  async getAll() {
+    const result = await axios.get(ENDPOINT_URL)
+    return result.data
+  },
+  async post(user: User) {
+    const result = await axios.post(ENDPOINT_URL, user)
+    return result.data
+  },
+  async delete(user: User) {
+    const result = await axios.delete(ENDPOINT_URL + "/" + user.id)
+    return result.data
+  },
+  async patch(user: User) {
+    const result = await axios.put(ENDPOINT_URL + "/" + user.id, user)
+    return result.data
+  },
+}
+
+export default userApi
